c2c0b30ed2c43185186582b3075664948dbf1e42
aljobiju <aljovadackel@gmail.com>
2024-01-30 18:42:52 +0530

    get calculations api done

--- src/routers/calculations.ts
+++ src/routers/calculations.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response, Router } from "express";
 import calculationsGet from "../controllers/calculations/getCalculations";
-import addCalculations from "../controllers/calculations/addCalculations";
+
 import getCalculations from "../controllers/calculations/getCalculations";
+import addCalculations from "../controllers/calculations/addCalculations";
 
 const calculationsRouter: Router = express.Router();
 //Routers for the calculations table
