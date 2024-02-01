f64cf1238832dc987c2c9cea4ffd4546e0e5da30
Hariprasad-j21 <hariprasad.j21@gmail.com>
2024-02-01 09:46:36 +0530

    added userStorySessionMapping

---
+++ types/modelTypes/userStorySessionMapping.ts
@@ -0,0 +1,12 @@
+import { Model } from "sequelize";
+
+class userStorySessionMapping extends Model {
+  public id!: number;
+  public userStoryId!: number;
+  public sessionId!: number;
+  public roundNumber!: number;
+  public comment?: Text;
+  public storyPointResult!: number;
+}
+
+export default userStorySessionMapping;
