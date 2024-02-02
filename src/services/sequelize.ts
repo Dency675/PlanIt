e03d9e8470c4e28b0cbd71d4bff5b8a301cf9c82
geevarghese2000 <geevarghesemvarghese@gmail.com>
2024-02-02 10:21:25 +0530

    fix: set sync force to false

--- src/services/sequelize.ts
+++ src/services/sequelize.ts
@@ -11,7 +11,7 @@ export const sequelizeSync = async (): Promise<void> => {
     });
 
   await sequelize
-    .sync({ force: true })
+    .sync({ force: false })
     .then(() => {
       console.log("Database synced");
     })
