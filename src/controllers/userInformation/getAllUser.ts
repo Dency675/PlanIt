5a060239f54bd7485ffcdc27a36ac14bcbf1e573
aljobiju <aljovadackel@gmail.com>
2024-02-08 11:35:13 +0530

    getAllUsers by pagination done

--- src/controllers/userInformation/getAllUser.ts
+++ src/controllers/userInformation/getAllUser.ts
@@ -1,18 +1,29 @@
 import { Request, Response } from "express";
 import userInformation from "../../models/userInformation";
-
+ 
 /**
- * Handles the retrieval of all user information from user information table
+ * Handles the retrieval of all user information from user information table with pagination
  *
  * @param {Request} req - Express Request object containing client data.
  * @param {Response} res - Express Response object for sending the server's response.
  * @returns {Promise<void>} A JSON response indicating the success or failure of the operation.
  */
-
+ 
 const getAllUsers = async (req: Request, res: Response): Promise<void> => {
   try {
-    const allUsers = await userInformation.findAll();
-
+ 
+    let { offset } = req.query;
+ 
+    let skip: number = 0;
+    if (offset) {
+      skip = parseInt(offset as string);
+    }
+ 
+    const allUsers = await userInformation.findAll({
+      limit: 7,
+      offset: skip,
+    });
+ 
     if (allUsers.length > 0) {
       res.status(200).json({
         message: "Data retrieved successfully",
@@ -27,9 +38,10 @@ const getAllUsers = async (req: Request, res: Response): Promise<void> => {
     console.error("Error in retrieving users:", error);
     res.status(500).json({
       message: "Internal Server Error",
-      error: error, 
+      error: error,
     });
   }
 };
-
+ 
 export default getAllUsers;
+ 
