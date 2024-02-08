acb056110871ed32c78558a5002b694c70d96ec7
Hariprasad-j21 <hariprasad.j21@gmail.com>
2024-02-08 13:07:25 +0530

    changes in getUserStoryCount

--- src/routers/scalesRouter.ts
+++ src/routers/scalesRouter.ts
@@ -1,16 +1,15 @@
-import express, { Request, Response,Router } from "express";
-import { postScales }from '../controllers/scales/postScales'
-import { getScalesByID }from '../controllers/scales/getScales'
+import express, { Request, Response, Router } from "express";
+import { postScales } from "../controllers/scales/postScales";
+import { getScalesByID } from "../controllers/scales/getScales";
 
-const router:Router = express.Router();
+const router: Router = express.Router();
 
-router.post("/postScales", async(req: Request, res: Response) => {
-    postScales(req,res);
+router.post("/postScales", async (req: Request, res: Response) => {
+  postScales(req, res);
 });
 
-router.get("/getScalesByID", async(req: Request, res: Response) => {
-    getScalesByID(req,res);
+router.get("/getScalesByID", async (req: Request, res: Response) => {
+  getScalesByID(req, res);
 });
-
 
 export default router;
