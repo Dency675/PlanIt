19cd99f97e9d1738324e5d11216d1a1d666c2309
Dency675 <neelamkavil2002@gmail.com>
2024-02-10 10:52:46 +0530

    added api getStoryPointByParticipants

--- src/routers/userStorySessionMapping.ts
+++ src/routers/userStorySessionMapping.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import addUserStorySessionMapping from "../controllers/userStorySessionMapping/addUserStorySessionMapping";
 import getUserStoryCountsBySession from "../controllers/userStorySessionMapping/getUserStoryCount";
 import getAllUserStoriesBySessionId from "../controllers/userStories/getAllUserStoriesBySessionId";
+import getStoryPointByParticipants from "../controllers/userStorySessionMapping/getStoryPointByParticipants";
 
 const userStorySesssionMappingRouter = express.Router();
 
@@ -23,6 +24,13 @@ userStorySesssionMappingRouter.get(
   "/getAllUserStoriesBySessionId",
   async (req: Request, res: Response) => {
     getAllUserStoriesBySessionId(req, res);
+  }
+);
+
+userStorySesssionMappingRouter.get(
+  "/getStoryPointByParticipants",
+  async (req: Request, res: Response) => {
+    getStoryPointByParticipants(req, res);
   }
 );
 
