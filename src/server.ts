ecf8173764d30b2eb082c021c8577bf3be46dd8d
Dency675 <neelamkavil2002@gmail.com>
2024-02-13 15:08:14 +0530

    conflict resolved

--- src/server.ts
+++ src/server.ts
@@ -29,107 +29,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3001;
-const Role = sequelize.define("Roled", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    // unique: true,
-  },
-});
-
-interface RoomAttributes {
-  roomId: string;
-  creator: string;
-}
-const Room = sequelize.define("Room", {
-  roomId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    primaryKey: true,
-  },
-  creator: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
-
-// app.post("/api/roles", async (req, res) => {
-//   try {
-//     const { name } = req.body;
-
-//     // Create a new role in the datasbase
-//     const role = await Role.create({ name });
-
-//     res.status(201).json(role);
-//   } catch (error) {
-//     console.error("Error creating role:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// });
-// app.post("/api/rooms", async (req, res) => {
-//   try {
-//     const { roomId, creator } = req.body;
-
-//     // Create a new role in the database
-//     const room = await Room.create({ roomId, creator });
-
-//     res.status(201).json(room);
-//   } catch (error) {
-//     console.error("Error creating role:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// });
-
-// app.get("/api/room/:roomId", async (req, res) => {
-//   try {
-//     const { roomId } = req.params;
-//     console.log(roomId);
-//     const room = await Room.findOne({ where: { roomId } });
-//     if (room) {
-//       res.status(200).json({ data: room });
-//     } else {
-//       res.status(404).json({ message: "Room not found" });
-//     }
-//   } catch (error) {
-//     console.error("Error fetching room:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// });
 
 sequelizeSync();
-
-// io.on("connection", (socket: Socket) => {
-//   console.log("New client connected");
-
-//   socket.on("createRoom", async (creator) => {
-//     try {
-//       const roomId = generateRoomId();
-
-//       console.log(creator);
-
-//       const currentTime = new Date().toLocaleTimeString();
-//       socket.join(roomId);
-
-//       io.to(roomId).emit("roomCreated", { roomId, currentTime, creator });
-
-//       await Promise.all([Room.create({ roomId: roomId, creator: creator })]);
-//     } catch (error) {
-//       console.error("Error creating room:", error);
-//     }
-//   });
-
-//   socket.on("joinRoom", (roomId: string, creator: string) => {
-//     socket.join(roomId);
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("Client disconnected");
-//   });
-// });
-
-// function generateRoomId(): string {
-//   return Math.random().toString(36).substring(7);
-// }
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -141,69 +42,15 @@ chooseRoutes(app);
 //   console.log(`Server is running on port ${port}`);
 // });
 
-app.post("/api/roles", async (req, res) => {
-  try {
-    const { name } = req.body;
-
-    // Create a new role in the database
-    const role = await Role.create({ name });
-
-    res.status(201).json(role);
-  } catch (error) {
-    console.error("Error creating role:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-app.post("/api/rooms", async (req, res) => {
-  try {
-    const { roomId, creator } = req.body;
-
-    // Create a new role in the database
-    const room = await Room.create({ roomId, creator });
-
-    res.status(201).json(room);
-  } catch (error) {
-    console.error("Error creating role:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-app.get("/api/room/:roomId", async (req, res) => {
-  try {
-    const { roomId } = req.params;
-    console.log(roomId);
-    const room = await Room.findOne({ where: { roomId } });
-    if (room) {
-      res.status(200).json({ data: room });
-    } else {
-      res.status(404).json({ message: "Room not found" });
-    }
-  } catch (error) {
-    console.error("Error fetching room:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
 io.on("connection", (socket: Socket) => {
   console.log("New client connected");
 
-  socket.on("createRoom", async (creator: any) => {
+  socket.on("createRoom", async (sessionId: string) => {
     try {
-      const roomId = generateRoomId();
+      socket.join(sessionId);
 
-      console.log("creator");
-      console.log(creator);
-
-      const currentTime = new Date().toLocaleTimeString();
-      socket.join(roomId);
-
-      io.to(roomId).emit("roomCreated", { roomId, currentTime, creator });
-
-      await Promise.all([
-        // Role.create({ name: "admin" }),
-        // Role.create({ name: "user" }),
-        Room.create({ roomId: roomId, creator: creator }),
-      ]);
+      io.to(sessionId).emit("roomCreated", {});
+      console.log(`Room created`);
     } catch (error) {
       console.error("Error creating room:", error);
     }
@@ -217,10 +64,6 @@ io.on("connection", (socket: Socket) => {
     console.log("Client disconnected");
   });
 });
-
-function generateRoomId(): string {
-  return Math.random().toString(36).substring(7);
-}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
